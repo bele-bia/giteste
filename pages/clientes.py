98b8e24fa83b123e416a25e765c05968480ce9a1
Teste <63551605+bele-bia@users.noreply.github.com>
2026-04-19 14:45:16 -0300

    Add files via upload

--- pages/clientes.py
+++ pages/clientes.py
@@ -5,7 +5,7 @@ import pandas as pd
 # --- ESTILIZAÇÃO VERDE ---
 st.markdown("""
     <style>
-    .stApp {background-color: #84a3bf;}
+    .stApp {background-color: #8ed0a8;}
     h1, h2, h3 {color: #006400;}
     .stButton>button {background-color: #228B22; color: white;}
     </style>
